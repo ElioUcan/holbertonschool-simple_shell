7f4d9ad5d70523496d584043c8a301fd402f1915
Elio Ucan <eliozapata2006@gmail.com>
2026-08-10 18:54:43 -0600

    commit

--- main.h
+++ main.h
@@ -9,7 +9,7 @@
 #include <unistd.h>
 #include  <stdio.h>
 
-int main(int argc, char *argv[]);
+int main(int argc, char *argv[], char *env[]);
 int uz_sh(pid_t ch_pid, char *buffer, char *args[], char *argv);
 int handle_execution(char *args[], char *env[], char *argv_0, char *buffer);
 void tokenize_input(char *buffer, char *args[]);

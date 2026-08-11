9ad0b8e89b9cab8e1ada75568c3b8de487deb231
Elio Ucan <eliozapata2006@gmail.com>
2026-08-10 19:13:19 -0600

    commit

--- main.c
+++ main.c
@@ -107,8 +107,14 @@ int handle_execution(char *args[], char *env[], char *argv_0, char *buffer)
 {
 char *path_result = NULL;
 pid_t ch_pid;
-int gb;
-if (access(args[0], X_OK) != 0)
+int gb, i = 0, has_slash = 0;
+while (args[0][i])
+{
+if (args[0][i] == '/')
+has_slash = 1;
+i++;
+}
+if (has_slash == 0)
 {
 path_result = uz_path(env, args);
 if (path_result == NULL)
@@ -117,6 +123,11 @@ fprintf(stderr, "%s: 1: %s: not found\n", argv_0, args[0]);
 return (127);
 }
 args[0] = path_result;
+}
+else if (access(args[0], X_OK) != 0)
+{
+fprintf(stderr, "%s: 1: %s: not found\n", argv_0, args[0]);
+return (127);
 }
 ch_pid = fork();
 gb = uz_sh(ch_pid, buffer, args, argv_0);
